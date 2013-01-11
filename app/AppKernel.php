1f6a0b1aff88269bf8f649d050bc9f1de5053536
Shaid <dr.shaid@gmail.com>
2013-01-11 15:45:54 +1100

    Install and setup StofDoctrineExtensions

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,10 +20,14 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             
+
+            
             new CMS\PageBundle\CMSPageBundle(),
             new CMS\SecurityBundle\CMSSecurityBundle(),
             new CMS\AdminBundle\CMSAdminBundle(),
             new CMS\SharedBundle\CMSSharedBundle(),
+            
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             
             new FOS\UserBundle\FOSUserBundle(),
             
@@ -41,7 +45,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
