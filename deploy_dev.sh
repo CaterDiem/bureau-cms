759366721213f72da95d4d535f91b0b56210e455
Shaid <dr.shaid@gmail.com>
2013-03-13 13:29:31 +1100

    Update deplyoment scripts to not clear each other's cache.

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,4 +1,5 @@
+rm -rf app/cache/dev/*
 app/console cache:clear --env dev
 app/console assets:install --env dev
 app/console assetic:dump --env dev
-chmod -R go+rw app/cache
+chmod -R go+rw app/cache/dev/
--- deploy_production.sh
+++ deploy_production.sh
@@ -1,4 +1,5 @@
+rm -rf app/cache/prod/*
 app/console cache:clear --env prod
 app/console assets:install --env prod
 app/console assetic:dump --env prod
-chmod -R go+rw app/cache
+chmod -R go+rw app/cache/prod/
