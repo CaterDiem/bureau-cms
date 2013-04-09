db9972e0ad05587191b838d644bb4d068696f5d5
Shaid <dr.shaid@gmail.com>
2013-04-09 16:35:37 +1000

    Blocks refactors to BlockCollection, as it is less confusing.
    Additionally, Collection will .save() to localStorage on a change of a member.

---
+++ src/CMS/PageBundle/Resources/public/js/collections/BlockCollection.js
@@ -0,0 +1,19 @@
+/* 
+ * BlockCollection
+ * Collection of block models
+ * 
+ * @author: jtemplet
+ * @version: 0.1
+ * @date: 2012/03/21
+ */
+
+var BlockCollection = Backbone.Collection.extend({
+    model: Block,
+    localStorage: new Backbone.LocalStorage("CMSPageCore.blocks.BlocksCollection"),
+    initialize: function(){
+         //this.fetch(); // load blocks from local storage.
+    },
+    changed: function(){
+        this.save();
+    }
+});
