5de6e9f0d3912b8054f69ededf25a21b5044ac78
Shaid <dr.shaid@gmail.com>
2013-04-22 18:39:13 +1000

    FOSRESTBundle: Config changed to default to JSON.
    NOTE: This probably breaks IE7 support. Care factor?

---
+++ src/CMS/PageBundle/Resources/public/js/page-pages.js
@@ -0,0 +1,54 @@
+/* 
+ * CMSPageCore
+ * Core functionality for the Page and Block editor. 
+ * 
+ * Why's this called Core? Naming things sucks.
+ * 
+ * @author: jtemplet
+ * @version: 0.1
+ * @date: 2012/02/06
+ */
+
+ // default to production. 
+var CMS_ENVIRONMENT = 'production';
+var CMS_REST_BASE_URL = '/web/';
+var CMS_DEBUG_STATE = '0';
+
+var BLOCK_URI = 'cms/page/blocks/';
+var BLOCK_INSTANCE_URI = 'cms/page/instances/';
+var PAGE_URI = 'cms/page/pages/';
+var TEMPLATE_URI = 'cms/page/templates/';
+
+var SELECTOR_EDITABLE_HTML_BLOCKS = '[cms-block-type="HTML"][cms-block-editable]';
+var SELECTOR_EDITABLE_LAYOUT_BLOCKS = '[cms-block-type="Layout"][cms-block-editable]';
+
+(function( CMSPageCore, $, undefined ) {
+    // properties
+
+    // functions
+    CMSPageCore.init = function() {        
+        // instantiate the core objects.
+        // this will also set the environment to development if we're on a development url
+        this.debug = CMSPageDebug.init();              
+        this.ui = CMSPageUI;
+        this.rest = CMSPageRest;
+        this.storage = CMSPageStorage;      
+        this.blocks = CMSPageBlocks;
+                       
+        // init block things.
+        // ensure server-generated blocks are in the BlockCollection        
+        //this.blocks.init();
+        
+        // set the editor deactivation handler to store blocks in localstorage.
+        this.ui.setGlobalEditorDeactivationHandler(CMSPageCore.blocks.storeBlockChangesLocally);
+
+        this.debug.log('READY PLAYER ONE!');
+        
+        
+    };
+    
+    // editor functions
+    CMSPageCore.attachEditor = function(element) {};
+
+}( window.CMSPageCore = window.CMSPageCore || {}, jQuery));
+
