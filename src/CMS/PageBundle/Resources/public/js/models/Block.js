985ccb143d5a4bbac679d36f32d326aa9a6d0e21
Shaid <dr.shaid@gmail.com>
2013-05-13 15:02:06 +1000

    CMSPageBundle:CMSPageBlock clone() implemented. Clones the model, and optionally deepClones relations based on deepCloneable property in relation.

--- src/CMS/PageBundle/Resources/public/js/models/Block.js
+++ src/CMS/PageBundle/Resources/public/js/models/Block.js
@@ -13,9 +13,11 @@ var Block = Backbone.RelationalModel.extend({
             key: 'children',
             relatedModel: 'Block',
             collectionType: 'BlockCollection',
+            deepCloneable: true,
             reverseRelation: {
                 key: 'parent',
-                includeInJSON: 'id'
+                includeInJSON: 'id',
+            
             }
         }],    
     schema: {
@@ -44,17 +46,57 @@ var Block = Backbone.RelationalModel.extend({
         description: 'Text',
         blockTemplate: {model: 'Template'} // note that this actually lives in blockInstance in the DB. haxx!
     },
-    initialize: function(options) {        
+    initialize: function(options) {
         //this.on('sync', this.onSync);
     },
-    onSync: function(){
-        
+    onSync: function() {
+
     },
-    generateElementId: function(){
-        if(this.get('element') == null){            
-            elementId = this.get('name')+"-"+this.get('id');
+    /**
+     * Create a new model with identical attributes to this one.
+     * Clones relationships that are defined in the deepCloneableRelations array.     
+     * @returns the deep clone
+     */
+    clone: function() {
+        var clone = Backbone.RelationalModel.prototype.clone.apply(this);
+
+        for (var attribute in this.attributes) {
+            // if this attribute is set as deepCloneable, process it.
+            if (this.getRelation(attribute) && this.getRelation(attribute).options.deepCloneable) {
+                var value = this.attributes[attribute];
+
+                // this deals with all hasOne relations
+                if (value && value instanceof Backbone.Model) {
+                    clone.attributes[attribute] = value.clone();
+                }
+
+                // this deals with all hasMany relations
+                if (value && value instanceof Backbone.Collection) {
+                    value.each(function(model) {
+                        clone.get(attribute).add(model.clone());
+                    });
+                }
+            }
+        }
+        // Block specific things.       
+        // change the name
+        clone.set('name', clone.get('name') + '-clone');
+
+        // unset the element, and regenerate
+        clone.unset('element');
+        clone.generateElementId();
+
+        return clone;
+    },
+    generateElementId: function() {
+        //if (this.get('element') == null) {
+            var id = this.get('id');
+            if (id == null) {
+                id = this.cid;
+            }
+            elementId = this.get('name') + "-" + id;
             this.set('element', elementId);
-        }                
+        //}
         return this;
     },
     /**
@@ -64,9 +106,9 @@ var Block = Backbone.RelationalModel.extend({
     setParent: function(parent) {
         // this ensures the parent block recognises this as a child block to this block.
         // 
-       
+
         CMSPageCore.debug.log('Block: ' + this.get('name') + ' parent set to: ' + parent.get('name') + '. Adding block as child to parent.');
-        
+
         this.set('parent', parent);
         return this;
     }
